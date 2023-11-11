bf789c11f3bd92ada1618ae782c1fbac5d5f6d76
Miranda12m <synysterteensier@outlook.es>
2023-11-11 17:30:40 -0600

    homescreenfix

--- vivalavida/src/screens/HomeScreen.js
+++ vivalavida/src/screens/HomeScreen.js
@@ -7,19 +7,60 @@ import Carousel from '../components/banner';
 import Services from '../components/services';
 function HomeScreen() {
   return (
-    <Container>
-      <br />
-      <br />
-      <br />
-      <h1>Home Screen bitch</h1>
-    </Container>
-    <div>
-      <Carousel/>
-      <h1>Bolas de Mono</h1>
-      {tourData.map((tour) => (
-        <Tour key={tour.id} tour={tour} />
-      ))}
-    </div>
+    <>
+      <Carousel />
+
+      <Container>
+
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            marginTop: '5%',
+            marginBottom: '5%',
+          }}
+        >
+          <Row>
+            <Col>
+              <div
+                style={{
+                  flex: 1,
+                  order: 2, // Cambiar el orden en dispositivos móviles
+
+                }}
+              >
+
+                <h2>About Us</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+                  ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                  anim id est laborum.
+                </p>
+              </div>
+            </Col>
+            <Col>
+              <div style={{ flex: 1, order: 1 }}>
+                <img
+                  src="https://images.unsplash.com/photo-1580934738416-ad531f2920f7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="About Us"
+                  style={{ maxWidth: '100%', minWidth: '300px', height: 'auto' }}
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
+
+      </Container >
+      <Services/>
+    </>
+
   );
 }
 
