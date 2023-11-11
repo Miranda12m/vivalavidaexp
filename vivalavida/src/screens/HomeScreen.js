73a9ceecbe0a910133f430cca7c6beae56966711
Miranda12m <synysterteensier@outlook.es>
2023-11-11 00:50:08 -0600

    componentsTest

--- vivalavida/src/screens/HomeScreen.js
+++ vivalavida/src/screens/HomeScreen.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Tour from '../components/Tour';
+import tourData from '../staticdata';
 
 function HomeScreen() {
   return (
     <div>
       <h1>Bolas de Mono</h1>
+      {tourData.map((tour) => (
+        <Tour key={tour.id} tour={tour} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
