41752d343cdeb77391cec9239895e16eeaa6cdd4
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2023-11-11 22:11:49 -0600

    AboutUs

--- vivalavida/src/screens/HomeScreen.js
+++ vivalavida/src/screens/HomeScreen.js
@@ -5,19 +5,22 @@ import { Container, Row, Col } from 'react-bootstrap';
 import InstagramEmbed from '../components/Instagram';
 import Carousel from '../components/banner';
 import Services from '../components/services';
+import { FaFacebook, FaTwitter, FaInstagram, FaEvernote } from 'react-icons/fa';
+import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
+
 function HomeScreen() {
   return (
     <>
       <Carousel />
-
+    <br></br>
       <Container>
-
         <div
           style={{
             display: 'flex',
             alignItems: 'center',
             marginTop: '5%',
             marginBottom: '5%',
+
           }}
         >
           <Row>
@@ -25,40 +28,61 @@ function HomeScreen() {
               <div
                 style={{
                   flex: 1,
-                  order: 2, // Cambiar el orden en dispositivos móviles
-
+                  order: 2,
                 }}
               >
+                <h2 style={{ marginTop: '4%' }}>About Us</h2>
+                <p style={{ marginTop: '4%' }}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                {/* Agrega el botón de redirección hacia "Contact Us" */}
+                <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+                  <button onClick={() => window.location.href = '/contact-us'} className="btn btn-primary" style={{ verticalAlign: 'center', margin: 'auto', alignSelf: 'center' }}>Contact Us</button>
 
-                <h2>About Us</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                  ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat.
-                </p>
-                <p>
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                  anim id est laborum.
-                </p>
+                </div>
+                <Row className="mt-5">
+                  <Col>
+                    <MDBBtn className='m-1' style={{ backgroundColor: '#3b5998' }} href='#'>
+                      <FaFacebook size={20} style={{ marginRight: '10px' }} /> Facebook
+                    </MDBBtn>
+                  </Col>
+                  <Col>
+                    <MDBBtn className='m-1' style={{ backgroundColor: '#55acee' }} href='#'>
+                      <FaTwitter size={20} style={{ marginRight: '10px' }} /> Twitter
+                    </MDBBtn>
+                  </Col>
+                  <Col>
+                    <MDBBtn className='m-1' style={{ backgroundColor: '#ac2bac' }} href='#'>
+                      <FaInstagram size={20} style={{ marginRight: '10px' }} />Instagram
+                    </MDBBtn>
+                  </Col>
+                  <Col>
+                    <MDBBtn className='m-1' style={{ backgroundColor: '#ff4500' }} href='#'>
+                    <FaEvernote size={20} style={{ marginRight: '10px' }} />Eventbrite
+                    </MDBBtn>
+                  </Col>
+                </Row>
               </div>
+
             </Col>
             <Col>
-              <div style={{ flex: 1, order: 1 }}>
-                <img
-                  src="https://images.unsplash.com/photo-1580934738416-ad531f2920f7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt="About Us"
-                  style={{ maxWidth: '100%', minWidth: '300px', height: 'auto', borderRadius: "2%" }}
-                />
-              </div>
+            <div style={{ flex: 1, order: 1, position: 'relative', marginLeft:'10%', marginTop:'5%' }}>
+              <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(207, 106, 0, 0.8)', transform: 'rotate(-7deg)' }}></div>
+              <img
+                src="https://images.unsplash.com/photo-1580934738416-ad531f2920f7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="About Us"
+                style={{ maxWidth: '87%', minWidth: '300px', height: 'auto', borderRadius: '2%', transform: 'rotate(7deg)' }}
+              />
+            </div>
             </Col>
           </Row>
-        </div>
 
-      </Container >
-      <Services/>
+        </div>
+      </Container>
+      <Services />
     </>
 
   );
