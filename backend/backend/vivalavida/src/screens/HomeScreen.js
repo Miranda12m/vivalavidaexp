ba3bead608098759526351e8a0edd1ab397e0f20
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2023-11-16 03:39:08 -0600

    commit

--- backend/backend/vivalavida/src/screens/HomeScreen.js
+++ backend/backend/vivalavida/src/screens/HomeScreen.js
@@ -56,22 +56,18 @@ function HomeScreen() {
                 <p>
                   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-                {/* Agrega el botón de redirección hacia "Contact Us" */}
-                <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+                {/*  <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
                   <button onClick={() => window.location.href = '/#/contact'} className="btn btn-primary button-hover" style={{ verticalAlign: 'center', margin: 'auto', alignSelf: 'center' }}>Contact Us</button>
 
-                </div>
+                </div> */}
+               
                 <Row className="mt-5" style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', marginBottom: '10%' }}>
                   <Col>
                     <button className='m-1 btn btn-primary button-hover' style={{ backgroundColor: '#3b5998' }} onClick={() => window.open('https://www.instagram.com/vivalavidaexperiences/', '_blank')}>
                       <FaFacebook size={20} style={{ marginRight: '10px' }} /> Facebook
                     </button>
                   </Col>
-                  <Col>
-                    <button className='m-1 btn btn-primary button-hover' style={{ backgroundColor: '#55acee' }} onClick={() => window.open('https://www.instagram.com/vivalavidaexperiences/', '_blank')}>
-                      <FaTwitter size={20} style={{ marginRight: '10px' }} /> Twitter
-                    </button>
-                  </Col>
+
                   <Col>
                     <button className='m-1 btn btn-primary button-hover' style={{ backgroundColor: '#ac2bac' }} onClick={() => window.open('https://www.instagram.com/vivalavidaexperiences/', '_blank')}>
                       <FaInstagram size={20} style={{ marginRight: '10px' }} />Instagram
@@ -91,7 +87,7 @@ function HomeScreen() {
                 <img
                   src="https://images.unsplash.com/photo-1580934738416-ad531f2920f7?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   alt="About Us"
-                  style={{ maxWidth: '87%', minWidth: '300px', height: 'auto', borderRadius: '2%' }}
+                  style={{ maxWidth: '87%', minWidth: '300px', height: 'auto', borderRadius: '2%', boxShadow:' 0px 0px 15px rgba(0, 0, 0, 0.3)' }}
                 />
               </div>
             </Col>
