96d5d8f5e5ec41e44c4b9792f3996e5dc4ac2d16
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2023-11-20 16:39:24 -0600

    asd

--- backend/backend/vivalavida/src/screens/services/HotelsScreen.js
+++ backend/backend/vivalavida/src/screens/services/HotelsScreen.js
@@ -24,38 +24,38 @@ function HotelsScreen() {
 
   return (
     <>
-    <BannerHot />
-    <Navbars />
-    <Container>
-      <h1
-        data-aos="fade-right"
-        style={{ borderBottom: "1px solid #2f2f2f", marginBottom: "15px" }}
-      >
-        <strong>Hotels</strong>
-      </h1>
-      <br />
+      <BannerHot />
+      <Navbars />
+      <Container>
+        <h1
+          data-aos="fade-right"
+          style={{ borderBottom: "1px solid #2f2f2f", marginBottom: "15px" }}
+        >
+          <strong>Hotels</strong>
+        </h1>
+        <br />
 
-      {/* Hotels Section */}
-      <Link to="{hotels.socials}">
-      <Row data-aos="zoom-in">
-        {hotels.map((hotels) => (
-          <Col key={hotels.id} xs={12} sm={6} md={4} lg={4}>
-            <Card>
-              <Card.Img variant="top" src={hotels.image} alt={hotels.title} />
-              <Card.Body>
-                <Card.Title>{hotels.title}</Card.Title>
-                <Card.Text>{hotels.description}</Card.Text>
-                <Card.Text>Location: {hotels.location}</Card.Text>
-                <a href={hotels.socials} target="_blank" rel="noopener noreferrer">
-                  Socials
-                </a>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      </Link>
-    </Container>
+        {/* Hotels Section */}
+        <Link to="{hotels.socials}" target="_blank" rel="noopener noreferrer">
+          <Row data-aos="zoom-in">
+            {hotels.map((hotels) => (
+              <Col key={hotels.id} xs={12} sm={6} md={4} lg={4}>
+                <Card>
+                  <Card.Img variant="top" src={hotels.image} alt={hotels.title} />
+                  <Card.Body>
+                    <Card.Title>{hotels.title}</Card.Title>
+                    <Card.Text>{hotels.description}</Card.Text>
+                    <Card.Text>Location: {hotels.location}</Card.Text>
+                    <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+                      <button className="btn btn-primary button-hover" type="button" style={{ verticalAlign: 'center', margin: 'auto', alignSelf: 'center' }}>See more</button>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Link>
+      </Container>
     </>
   );
 }
