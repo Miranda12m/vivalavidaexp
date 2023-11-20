977a6ae8cbe9a94971f27466800dbb9b1ae0eeea
GabrielAgundiz <gabrielg.agundizh@gmail.com>
2023-11-20 02:35:27 -0600

    asd

--- backend/backend/vivalavida/src/components/services/Restaurant.jsx
+++ backend/backend/vivalavida/src/components/services/Restaurant.jsx
@@ -30,29 +30,34 @@ function Restaurant({ restaurant }) {
                 `}
       </style>
       <Container>
-      <div style={{borderBottom:'1px solid #2f2f2f'}}>
-        <h2><strong>{restaurant.title}</strong></h2>
-        <p style={{marginLet:'20%', marginRight:'20%'}}> {restaurant.description}</p>
-        <Row style={{marginTop:'5%', marginBottom:'5%', marginLeft:'10%', marginRight:'10%', verticalAlign: 'center', margin: 'auto', alignSelf: 'center'}}>
-          <Col>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-              <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-              <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" style={{marginRight:'4px'}}/>
-            </svg>
-              {restaurant.location}  
-          </Col>
-        </Row>
-        <Link to={restaurant.socials} >
-              <button className="btn btn-primary button-hover" type="button" style={{ verticalAlign: 'center', margin: 'auto', alignSelf: 'center' }}>See more</button>
-            </Link>
-        <br></br>
-        <div style={{ verticalAlign: 'center', margin: 'auto', alignSelf: 'center' }}> 
-        <img src={restaurant.image} position='top' style={{ borderRadius: "1.2%", width: "100wh", minWidth:'400px', objectFit:'cover', height:'50vh', verticalAlign: 'center', margin: '30px', alignSelf: 'center', boxShadow:' 0px 0px 15px rgba(0, 0, 0, 0.3)' }} alt={restaurant.title}></img>
+        <div style={{ borderBottom: '1px solid #2f2f2f' }}>
+          <h2 style={{ textAlign: 'center' }}><strong>{restaurant.title}</strong></h2>
+          <p style={{ marginLeft: '20%', marginRight: '20%' }}>{restaurant.description}</p>
+          <Row style={{ marginTop: '5%', marginBottom: '5%', marginLeft: '10%', marginRight: '10%', alignItems: 'center' }}>
+            <Col>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt" viewBox="0 0 16 16" style={{ marginRight: '4px' }}>
+                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+              </svg>
+              <span style={{ marginRight: '10px' }}>{restaurant.location}</span>
+            </Col>
+          </Row>
+          <br />
+          <br />
+          <div style={{ textAlign: 'center' }}>
+            <img
+              src={restaurant.image}
+              style={{ borderRadius: "1.2%", width: "100%", minWidth: '400px', objectFit: 'cover', height: '50vh', margin: '30px', boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.3)' }}
+              alt={restaurant.title}
+            />
+          </div>
+          <Link to={restaurant.socials}>
+            <button className="btn btn-primary button-hover" type="button" style={{ display: 'block', margin: 'auto' }}>See more</button>
+          </Link>
         </div>
-      </div>
-
-      <br></br>
+        <br />
       </Container>
+
     </>
   );
 }
